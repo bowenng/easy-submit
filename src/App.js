4892f86d463c07713aa22e6e712c0a08e9d9142c
TAMA <tamajessica18@gmail.com>
2019-11-23 11:55:56 -0500

    preview

--- src/App.js
+++ src/App.js
@@ -1,63 +1,97 @@
 import React from 'react';
 import ImageEditor from './ImageEditor.js';
-import PreviewList from './PreviewList.js';
+import Preview from './Preview.js';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import ReactDOM from 'react-dom';
+import { Carousel } from 'react-responsive-carousel';
+//import PreviewList from './PreviewList.js';
 
 class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             images: []
         }
+
         this.addImage = this.addImage.bind(this);
         this.updateOrder = this.updateOrder.bind(this);
     }
 
-    addImage(imageUrl){
-        this.setState((prevState, prevProps)=>{
+    addImage(imageUrl) {
+        this.setState((prevState, prevProps) => {
             const images = prevState.images.slice();
             images.push(imageUrl);
-            
-            return {images:images};
-        })
-    }
 
-    removeImage(i){
-        this.setState((prevState, prevProps)=>{
-            const images = prevState.images.slice();
-            
             return { images: images };
         })
     }
-    updateOrder(newImages){
-        this.setState({images:newImages})
+
+    removeImage(i) {
+        this.setState((prevState, prevProps) => {
+            const images = prevState.images.slice();
+
+            return { images: images };
+        })
+    }
+    updateOrder(newImages) {
+        this.setState({ images: newImages })
     }
 
-    render () {
-        const images = this.state.images;
-        const previews = images.map(
-            (imageUrl, i) => (<li>
-                <img key={i} src={imageUrl} alt="none"/>
-             </li>)
-        );
-        console.log(previews);
-        
-        return (
-          <div className="App">
-              <main className="container">
-                  <div className="split left">
-                      <ol>
+    handlePreviewClick(index) {
+        const list = [...this.state.images]
+        this.setState({
+            preview: [list[index]]
+        })
+    }
 
-                            {previews}
-                      </ol>
-                  </div>
-                  <div className="split right">
-                    <ImageEditor addImage={this.addImage}/>
-                  </div>
-              </main>
-              
-          </div>
-      );
+
+    updateCurrentSlide(index) {
+        const currentSlide = this.state.currentSlide;
+
+        if (currentSlide !== index) {
+            this.setState({
+                currentSlide: index,
+            });
+        }
+    }
+
+
+    getPreview() {
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <main className="container">
+                    <div className="split left">
+                        
+                        <Carousel emulateTouch = {true} className = "carousel">
+                                {
+                                    this.state.images.map((imageUrl, i) => {
+                                        return (
+                                            <div className="preview block in">
+                                                <img class="preview" src={imageUrl} alt={"none"}  />
+                                                <input className="legend" type="text"/>
+                                            </div>
+
+                                        )
+                                    })
+                                }
+                            </Carousel>
+                                                       
+                            
+                       
+                    </div>
+                    <div className="split right">
+                        <ImageEditor addImage={this.addImage} />
+                    </div>
+                </main>
+
+            </div>
+        );
     }
 }
 
 export default App;
+
